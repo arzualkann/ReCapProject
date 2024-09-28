68ab39e93a99c4038d58eb16ff7d9ed6ac514b8b
arzualkann <arzualkann@outlook.com>
2024-09-29 01:56:17 +0300

    WebApi katmanı ve Controllerların eklenmesi

---
+++ WebAPI/Program.cs
@@ -0,0 +1,43 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSingleton<IBrandService,BrandManager>();
+builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
+
+builder.Services.AddSingleton<ICarService, CarManager>();
+builder.Services.AddSingleton<ICarDal, EfCarDal>();
+
+builder.Services.AddSingleton<IColorService, ColorManager>();
+builder.Services.AddSingleton<IColorDal, EfColorDal>();
+
+builder.Services.AddSingleton<ICustomerService, CustomerManager>();
+builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
+
+builder.Services.AddSingleton<IRentalService, RentalManager>();
+builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
+
+builder.Services.AddSingleton<IUserService, UserManager>();
+builder.Services.AddSingleton<IUserDal, EfUserDal>();
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
